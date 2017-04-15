7d5ea7feaf25cb99846bb9ad2f6d29cd6d7009ad
Samuel <foreversunyao@yahoo.com>
2017-04-15 10:18:11 +0800

    Update cronjob.sh

--- docker/cronjob.sh
+++ docker/cronjob.sh
@@ -4,7 +4,7 @@ docker exec -it $1 python /crawler/gnp/gnp_search.py GOOG
 docker exec -it $1 python /crawler/elasticsearch_article/article.py
 docker exec -it $1 python /crawler/elasticsearch_article/article_ana.py
 
-10 * * * * docker exec -it 62f523178289 sh /crawler/stockinfo_collect/collect_daily.sh
-10 * * * 2 docker exec -it 62f523178289 python /crawler/elasticsearch_article/article.py
-22 * * * 1 sh /home/ubuntu/usstock/docker/gnp_search.sh
-10 * * * 2 docker exec -it 62f523178289 python /crawler/elasticsearch_article/article_ana.py
+10 * * * * docker exec 58869f6dd7a6 sh /crawler/stockinfo_collect/collect_daily.sh
+10 * * * 2 docker exec  58869f6dd7a6 python /crawler/elasticsearch_article/article.py
+22 * * * 1 sh /data/usstock/usstock/docker/search_gnp.sh 58869f6dd7a6
+10 * * * 2 docker exec 58869f6dd7a6 python /crawler/elasticsearch_article/article_ana.py
